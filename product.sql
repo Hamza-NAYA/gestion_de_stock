697072297715aa2b1157d23ba36b72f058089325
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-02-08 10:37:03 +0100

    ajout d'un requirements + main

--- product.sql
+++ product.sql
@@ -7,3 +7,20 @@ CREATE TABLE IF NOT EXISTS product (
     id_category INT,
     FOREIGN KEY (id_category) REFERENCES category(id)
 );
+
+INSERT INTO product (name, description, price, quantity, id_category) VALUES
+    ('Ordinateur', 'Leger et puissant', 1000, 50, 1),
+    ('T-shirt', '100% coton', 20, 100, 2),
+    ('Programmer pour les nuls', 'Apprenez le code avec facilité', 30, 30, 3);
+
+SELECT * FROM product;
+
+resultat:
++----+--------------------------+--------------------------------+-------+----------+-------------+
+| id | name                     | description                    | price | quantity | id_category |
++----+--------------------------+--------------------------------+-------+----------+-------------+
+|  1 | Ordinateur               | Leger et puissant              |  1000 |       50 |           1 |
+|  2 | T-shirt                  | 100% coton                     |    20 |      100 |           2 |
+|  3 | Programmer pour les nuls | Apprenez le code avec facilité |    30 |       30 |           3 |
++----+--------------------------+--------------------------------+-------+----------+-------------+
+3 rows in set (0.00 sec)
